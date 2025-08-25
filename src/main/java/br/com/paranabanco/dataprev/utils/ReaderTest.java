d2fbe5a5e5c310cc00018410a505be6adabf99aa
Victor Leandro <victorleandro100@outlook.com>
2025-08-25 13:55:41 -0300

    Substitui Exception genérica por IOException e EmptyFileException

--- src/main/java/br/com/paranabanco/dataprev/utils/ReaderTest.java
+++ src/main/java/br/com/paranabanco/dataprev/utils/ReaderTest.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 
 import br.com.paranabanco.dataprev.domain.LinhaDoArquivo;
 
+import java.io.IOException;
 import java.util.List;
 
 @Component
@@ -15,17 +16,19 @@ public class ReaderTest implements CommandLineRunner {
     private Reader reader;
 
     @Override
-    public void run(String... args) throws Exception {
+    public void run(String... args) {
         System.out.println("--- INICIANDO LEITURA BRUTA DO ARQUIVO ---");
 
         try {
             List<LinhaDoArquivo> linhas = reader.lerLinhasDoArquivo();
-            
+
             System.out.println("Conteúdo lido do arquivo:");
             linhas.forEach(System.out::println);
 
-        } catch (Exception e) {
-            System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+        } catch (EmptyFileException e) {
+            System.err.println("Arquivo vazio: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("Erro de I/O ao ler o arquivo: " + e.getMessage());
         }
         
         System.out.println("--- LEITURA FINALIZADA ---");
